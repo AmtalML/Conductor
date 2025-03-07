1ed82008d83ec29c4fa59293d7501adaf26f058b
EC2 Default User <ec2-user@ip-172-31-36-110.ec2.internal>
2025-03-07 22:14:54 +0000

    Run script fixed

--- run.sh
+++ run.sh
@@ -15,10 +15,10 @@ fi
 echo "Installing dependencies..."
 if [[ "$OS" == "ubuntu" ]]; then
     sudo apt update
-    sudo apt install -y build-essential g++ cmake wget bzip2
+    sudo apt install -y build-essential g++ cmake wget bzip2 libboost-all-dev
 elif [[ "$OS" == "amzn" || "$OS" == "centos" || "$OS" == "rhel" ]]; then
     sudo yum groupinstall -y "Development Tools"
-    sudo yum install -y gcc gcc-c++ cmake wget bzip2
+    sudo yum install -y gcc gcc-c++ cmake wget bzip2 boost-devel
 elif [[ "$OS" == "macos" ]]; then
     brew install cmake boost
     exit 0
@@ -28,34 +28,26 @@ fi
 
 if [[ "$OS" != "macos" ]]; then
     if ! command -v nvcc &> /dev/null; then
-        wget -q https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
-        sudo dpkg -i cuda-keyring_1.0-1_all.deb
+        CUDA_REPO="https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb"
+        wget -q "$CUDA_REPO" -O cuda-keyring.deb
+        sudo dpkg -i cuda-keyring.deb
         sudo apt update
         sudo apt install -y cuda
         export PATH=/usr/local/cuda/bin:$PATH
     fi
 fi
 
-BOOST_VERSION="1.84.0"
-BOOST_VERSION_UNDERSCORE="${BOOST_VERSION//./_}"
-BOOST_DIR="boost_$BOOST_VERSION_UNDERSCORE"
-BOOST_TARBALL="https://boostorg.jfrog.io/artifactory/main/release/$BOOST_VERSION/source/$BOOST_DIR.tar.bz2"
-
-if [[ "$OS" != "macos" ]]; then
-    wget -q --show-progress "$BOOST_TARBALL"
-    tar --bzip2 -xf "$BOOST_DIR.tar.bz2"
-
-    cd "$BOOST_DIR"
-    ./bootstrap.sh --prefix=/usr/local
-    ./b2 install -j$(nproc)
-    cd ..
-    rm -rf "$BOOST_DIR" "$BOOST_DIR.tar.bz2"
-fi
-
 mkdir -p build && cd build
 cmake ..
 make -j$(nproc)
 
-./batching_service &
-./conductor_server
+if [[ -f batching_service && -f conductor_server ]]; then
+    ./batching_service &
+    ./conductor_server
+else
+    echo "Error: Executables not found!"
+    exit 1
+fi
 
+./conductor_server
+./batching_service
---
+++ server/main.cpp
@@ -0,0 +1,8 @@
+#include "include/utils.hpp"
+
+
+int main(int argc, char* argv[]) {
+
+
+    return 0;
+}
