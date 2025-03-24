c19f2412bc95c096721838509b96f52209535d5e
EC2 Default User <ec2-user@ip-172-31-24-61.ec2.internal>
2025-03-24 01:11:33 +0000

    adding tcp server

--- server/include/server.hpp
+++ server/include/server.hpp
@@ -1,10 +1,25 @@
 #ifndef SERVER_HPP
 #define SERVER_HPP
 
+#include <boost/asio.hpp>
+
+namespace beast = boost::beast;
+namespace http = beast:: http;
+namespace net = boost::asio;
+using tcp = net::ip::tcp;
+
 namespace Amtal {
 
 class Server {
 
+    net::io_context& ioc_;
+    tcp::acceptor acceptor_;
+
+public:
+
+
+
+private:
 
 
 
