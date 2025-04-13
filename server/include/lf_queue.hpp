e4717df6674a003fb5e23dc3c8746c99cca4d9f8
EC2 Default User <ec2-user@ip-172-31-77-115.ec2.internal>
2025-04-13 06:31:08 +0000

    lf queue

---
+++ server/include/lf_queue.hpp
@@ -0,0 +1,21 @@
+#ifndef LF_QUEUE_HPP
+#define LF_QUEUE_HPP
+
+#include "macros.hpp"
+#include <atomic>
+
+namespace Server {
+
+template<typename T>
+class LF_Queue final {
+
+
+
+
+
+    
+};
+
+}
+
+#endif
