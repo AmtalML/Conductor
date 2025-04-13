4fa3e0121d86b9c187a5f62ac1cfdc3947766097
EC2 Default User <ec2-user@ip-172-31-77-115.ec2.internal>
2025-04-13 05:04:31 +0000

    added macros for errors

---
+++ server/include/macros.hpp
@@ -0,0 +1,55 @@
+#ifndef MACROS_HPP
+#define MACROS_HPP
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+namespace Server {
+
+    inline auto ASSERT(bool cond, const std::string& msg) noexcept {
+        // todo: add debug/release flag to optimise this out in release builds
+        if (!cond) [[unlikely]] {
+            std::cerr << msg << "\n";
+            exit(EXIT_FAILURE);
+        }
+    }
+    
+    inline auto FATAL(const std::string& msg) noexcept {
+        std::cerr << msg << "\n";
+        exit(EXIT_FAILURE);
+    }
+    
+    /**
+     * @brief Check if an element does not exist in a vector
+     * @return True when the element is not in the vector
+     */
+    template<typename T>
+    inline bool element_does_not_exist(std::vector<T>& v, T element) {
+        return std::find(v.begin(), v.end(), element) == v.end();
+    }
+    
+    /**
+     * @brief Delete default ctor and copy/move ctor and assignment
+     * operators for a given classname
+     */
+    #define DELETE_DEFAULT_COPY_AND_MOVE(ClassName) \
+    public: \
+        ClassName() = delete; \
+        ClassName(const ClassName&) = delete; \
+        ClassName(const ClassName&&) = delete; \
+        ClassName& operator=(const ClassName&) = delete; \
+        ClassName& operator=(const ClassName&&) = delete;
+    
+    /**
+     * @brief Qualify class members as public if under test, else private.
+     */
+    #ifdef IS_TEST_SUITE
+    #define PRIVATE_IN_PRODUCTION public:
+    #else
+    #define PRIVATE_IN_PRODUCTION private:
+    #endif
+
+}
+
+#endif 
